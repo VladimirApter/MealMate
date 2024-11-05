23df22819782a6c97f55d460290b991b0987c510
VladimirApter <vova.apter@gmail.com>
2024-11-05 23:46:29 +0500

    Add restaurant data updating (not working without delete api request)

--- frontend/Business/tg_business_bot/main.py
+++ frontend/Business/tg_business_bot/main.py
@@ -1,7 +1,7 @@
 from telebot import types
 from Config import *
 from Registration import register_commands
-from excel_tables_work.create_table_template import create_menu_template
+from excel_tables_work.create_menu_template import create_menu_template
 
 create_menu_template()
 register_commands()
