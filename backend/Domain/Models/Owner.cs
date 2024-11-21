6bca55a0055ed19a240ed27ebe5689f6c98083e3
Yura Romashov <yura.romashov000@mail.ru>
2024-11-21 20:14:11 +0500

    add restaurant_ids owner get

--- backend/Domain/Models/Owner.cs
+++ backend/Domain/Models/Owner.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity;
 using System.Text.Json.Serialization;
 using Domain.DataBaseAccess;
+using Microsoft.EntityFrameworkCore;
 
 namespace Domain.Models;
 
