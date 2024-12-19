9d6ac55e8307e4db1f7c07819190324783d925cd
VladimirApter <vova.apter@gmail.com>
2024-12-20 03:42:05 +0500

    Fix fill menu template

--- backend/API/tg_bots/tg_business_bot/excel_tables_work/fill_menu_template.py
+++ backend/API/tg_bots/tg_business_bot/excel_tables_work/fill_menu_template.py
@@ -9,26 +9,34 @@ def fill_menu_template(template_path, output_file_path, menu: Menu):
     dishes_sheet = wb["Блюда"]
     drinks_sheet = wb["Напитки"]
 
-    row_num = 3
+    drink_row_num = 3
+    dish_row_num = 3
 
     for category in menu.categories:
         for item in category.menu_items:
-            sheet, weight_or_volume = _get_sheet_and_weight_or_volume(item, dishes_sheet, drinks_sheet)
+            sheet, weight_or_volume, is_drink = _get_sheet_and_weight_or_volume(
+                item, dishes_sheet, drinks_sheet)
             if sheet is None:
                 continue
 
-            _fill_row(sheet, row_num, category, item, weight_or_volume)
-            row_num += 1
+            if is_drink:
+                _fill_row(sheet, drink_row_num, category, item,
+                          weight_or_volume)
+                drink_row_num += 1
+            else:
+                _fill_row(sheet, dish_row_num, category, item,
+                          weight_or_volume)
+                dish_row_num += 1
 
     wb.save(output_file_path)
 
 
 def _get_sheet_and_weight_or_volume(item, dishes_sheet, drinks_sheet):
     if isinstance(item, Drink):
-        return drinks_sheet, item.volume
+        return drinks_sheet, item.volume, True
     elif isinstance(item, Dish):
-        return dishes_sheet, item.weight
-    return None, None
+        return dishes_sheet, item.weight, False
+    return None, None, False
 
 
 def _fill_row(sheet, row_num, category, item, weight_or_volume):
