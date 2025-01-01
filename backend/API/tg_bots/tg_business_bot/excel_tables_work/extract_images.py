4fd4f7a9c73c36537c18d2b2946d829f2fa7ea7a
VladimirApter <vova.apter@gmail.com>
2025-01-02 02:50:04 +0500

    Add dynamic compression quality adjustment

--- backend/API/tg_bots/tg_business_bot/excel_tables_work/extract_images.py
+++ backend/API/tg_bots/tg_business_bot/excel_tables_work/extract_images.py
@@ -7,9 +7,22 @@ from PIL import Image
 from tg_business_bot.Config import menu_item_images_dir
 
 
-def compress_image(input_path, output_path, quality=75):
+def compress_image(input_path, output_path, quality):
     with Image.open(input_path) as img:
         img.save(output_path, optimize=True, quality=quality)
+
+
+def determine_compression_quality(file_size):
+    if file_size < 50 * 1024:
+        return 95
+    elif file_size < 100 * 1024:
+        return 85
+    elif file_size < 200 * 1024:
+        return 75
+    elif file_size < 500 * 1024:
+        return 65
+    else:
+        return 55
 
 
 def extract_and_save_images(file_path):
@@ -31,14 +44,15 @@ def extract_and_save_images(file_path):
 
         for file_info in image_files:
             z.extract(file_info, temp_folder)
-
             src_path = os.path.join(temp_folder, file_info.filename)
             file_extension = os.path.splitext(file_info.filename)[1]
-
             unique_filename = f"{uuid.uuid4()}{file_extension}"
             dst_path = os.path.join(output_folder, unique_filename)
 
-            compress_image(src_path, dst_path)
+            file_size = os.path.getsize(src_path)
+            quality = determine_compression_quality(file_size)
+            compress_image(src_path, dst_path, quality)
+
             image_paths.append(unique_filename)
 
     shutil.rmtree(temp_folder)
